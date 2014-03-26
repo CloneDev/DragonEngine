d07627d82c20462b7b45876c5a4eaae569b5bcbf
sensorreinigung <jens@richter-fam.de>
2014-03-26 14:49:20 +0100

    Transformation ported

--- Engine/Entities/Camera.cs
+++ Engine/Entities/Camera.cs
@@ -16,6 +16,7 @@ namespace DragonEngine.Entities
         private Rectangle mViewport;
         private Rectangle mScreenViewport;
         private Matrix mViewportTransform;
+        private Matrix mScreenScale;
         private Matrix mScreenTransform;
 
         #endregion
@@ -24,7 +25,11 @@ namespace DragonEngine.Entities
 
         public Camera()
         {
-            //mViewport = new Rectangle(0, 0, (int)Game1.resolution.X, (int)Game1.resolution.Y);
+        }
+
+        public Camera(Rectangle pViewArea)
+        {
+            mViewArea = pViewArea;
         }
 
         #endregion
@@ -33,80 +38,74 @@ namespace DragonEngine.Entities
 
         public virtual void Initialize()
         {
-            mScreenViewport = new Rectangle();
+            mViewport = new Rectangle(0, 0, EngineSettings.VirtualResX, EngineSettings.VirtualResY);
+            mScreenViewport = new Rectangle(0, 0, EngineSettings.ScreenResX, EngineSettings.ScreenResY);
+            mScreenScale = Matrix.CreateScale((float)mScreenViewport.Width / EngineSettings.VirtualResX, (float)mScreenViewport.Height / EngineSettings.VirtualResY, 1);
+            mScreenTransform = mScreenScale * Matrix.CreateTranslation(mScreenViewport.X, mScreenViewport.Y, 0);
         }
 
-        //public virtual void Update(Player player, Map map)
-        //{
-        //    if (mViewport.X + leftspace > player.position.X) //Scrolling nach links
-        //    {
-        //        mViewport.X = (int)player.position.X - leftspace;
-        //    }
-        //    else if (mViewport.X + mViewport.Width - rightspace < player.position.X) //Scrolling nach rechts
-        //    {
-        //        mViewport.X = (int)player.position.X - (mViewport.Width - rightspace);
-        //    }
-        //    if (mViewport.X < 0) //Linker Maprand
-        //    {
-        //        mViewport.X = 0;
-        //    }
-        //    else if (mViewport.X > map.size.X - mViewport.Width) //Rechter Maprand
-        //    {
-        //        mViewport.X = (int)map.size.X - mViewport.Width;
-        //    }
+        public virtual void Update(Player player, Map map)
+        {
+            if (mViewport.X + leftspace > player.position.X) //Scrolling nach links
+            {
+                mViewport.X = (int)player.position.X - leftspace;
+            }
+            else if (mViewport.X + mViewport.Width - rightspace < player.position.X) //Scrolling nach rechts
+            {
+                mViewport.X = (int)player.position.X - (mViewport.Width - rightspace);
+            }
+            if (mViewport.X < 0) //Linker Maprand
+            {
+                mViewport.X = 0;
+            }
+            else if (mViewport.X > map.size.X - mViewport.Width) //Rechter Maprand
+            {
+                mViewport.X = (int)map.size.X - mViewport.Width;
+            }
 
-        //    if (mViewport.Y + topspace > player.position.Y) //Scrolling nach oben
-        //    {
-        //        mViewport.Y = (int)player.position.Y - topspace;
-        //    }
-        //    else if (mViewport.Y + mViewport.Height - bottomspace < player.position.Y) //Scrolling nach unten
-        //    {
-        //        mViewport.Y = (int)player.position.Y - (mViewport.Height - bottomspace);
-        //    }
-        //    if (mViewport.Y < 0) //Oberer Maprand
-        //    {
-        //        mViewport.Y = 0;
-        //    }
-        //    else if (mViewport.Y > map.size.Y - mViewport.Height) //Unterer Maprand
-        //    {
-        //        mViewport.Y = (int)map.size.Y - mViewport.Height;
-        //    }
-        //    UpdateTransformation(); //Abgekapselt damit Camera für Menü ohne Spieler verwendbar ist.
-        //}
+            if (mViewport.Y + topspace > player.position.Y) //Scrolling nach oben
+            {
+                mViewport.Y = (int)player.position.Y - topspace;
+            }
+            else if (mViewport.Y + mViewport.Height - bottomspace < player.position.Y) //Scrolling nach unten
+            {
+                mViewport.Y = (int)player.position.Y - (mViewport.Height - bottomspace);
+            }
+            if (mViewport.Y < 0) //Oberer Maprand
+            {
+                mViewport.Y = 0;
+            }
+            else if (mViewport.Y > map.size.Y - mViewport.Height) //Unterer Maprand
+            {
+                mViewport.Y = (int)map.size.Y - mViewport.Height;
+            }
+            UpdateTransformation(); //Abgekapselt damit Camera für Menü ohne Spieler verwendbar ist.
+        }
 
-        //public void UpdateTransformation()
-        //{
-        //    int width = EngineSettings.Graphics.GraphicsDevice.PresentationParameters.BackBufferWidth;
-        //    int height = EngineSettings.Graphics.GraphicsDevice.PresentationParameters.BackBufferHeight;
-        //    if (Game1.stretch) //Viewport screenfüllend
-        //    {
-        //        mScreenViewport.X = 0;
-        //        mScreenViewport.Y = 0;
-        //        mScreenViewport.Width = width;
-        //        mScreenViewport.Height = height;
-        //    }
-        //    else //Viewport mit Offset auf Screen
-        //    {
-        //        if (mScreenViewport.X < mScreenViewport.Y) //Balken oben/unten
-        //        {
-        //            mScreenViewport.Width = (int)width;
-        //            mScreenViewport.Height = (int)(width / Game1.resolution.X * Game1.resolution.Y);
-        //        }
-        //        else //Balken links/rechts
-        //        {
-        //            mScreenViewport.Height = (int)height;
-        //            mScreenViewport.Width = (int)(height / Game1.resolution.Y * Game1.resolution.X);
-        //        }
-        //        mScreenViewport.X = (width - (int)mScreenViewport.Width) / 2;
-        //        mScreenViewport.Y = (height - (int)mScreenViewport.Height) / 2;
-        //        //= viewport.Width / resolution.X;
-        //        //= viewport.Height / resolution.Y;
-        //    }
-        //    Matrix screenScale = Matrix.CreateScale((float)mScreenViewport.Width / Game1.resolution.X, (float)mScreenViewport.Height / Game1.resolution.Y, 1);
-        //    mScreenTransform = screenScale * Matrix.CreateTranslation(mScreenViewport.X, mScreenViewport.Y, 0);
+        public void UpdateViewportTransformation()
+        {
+            mViewportTransform = Matrix.CreateTranslation(-mViewport.X, -mViewport.Y, 0);
+        }
 
-        //    mViewportTransform = Matrix.CreateTranslation(-mViewport.X, -mViewport.Y, 0);
-        //}
+        //public void InitializeBlackBandTransformation()
+        {
+            if (mScreenViewport.X < mScreenViewport.Y) //Balken oben/unten
+            {
+                mScreenViewport.Width = (int)EngineSettings.ScreenResX;
+                mScreenViewport.Height = (int)(EngineSettings.ScreenResX / EngineSettings.VirtualResX * EngineSettings.VirtualResY);
+            }
+            else //Balken links/rechts
+            {
+                mScreenViewport.Height = (int)EngineSettings.ScreenResY;
+                mScreenViewport.Width = (int)(EngineSettings.ScreenResY / EngineSettings.VirtualResY * EngineSettings.VirtualResX);
+            }
+            mScreenViewport.X = (EngineSettings.ScreenResX - (int)mScreenViewport.Width) / 2;
+            mScreenViewport.Y = (EngineSettings.ScreenResY - (int)mScreenViewport.Height) / 2;
+
+            mScreenScale = Matrix.CreateScale((float)mScreenViewport.Width / EngineSettings.VirtualResX, (float)mScreenViewport.Height / EngineSettings.VirtualResY, 1);
+            mScreenTransform = mScreenScale * Matrix.CreateTranslation(mScreenViewport.X, mScreenViewport.Y, 0);
+            //mViewportTransform = Matrix.CreateTranslation(-mViewport.X, -mViewport.Y, 0);
+        }
 
         #endregion
 
