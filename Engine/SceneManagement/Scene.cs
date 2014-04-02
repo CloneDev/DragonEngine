e154d08e515ee2df82fc74092c479e312b36f98d
sensorreinigung <jens@richter-fam.de>
2014-04-02 10:32:01 +0200

    Scene reUPDATED
    Gleichzeitig das gleiche gemacht...

--- Engine/SceneManagement/Scene.cs
+++ Engine/SceneManagement/Scene.cs
@@ -20,16 +20,10 @@ namespace DragonEngine.SceneManagement
         protected Camera mCamera;
 
         protected Color mClearColor = Color.LawnGreen;
-
-        //// String1 für AssetName, String2 für AssetPfad
-        //protected List<GameObject> mUpdateGameObjects = new List<GameObject>();
-        //protected List<Action<GameTime>> mUpdateAction = new List<Action<GameTime>>();
-        //protected List<Action> mDrawAction = new List<Action>();
         #endregion
 
         #region Getter & Setter
         public String Name { get { return this.mName; } }
-        //public List<GameObject> UpdateGameObjects { get { return mUpdateGameObjects; } }
         public String Background { set { mBackgroundName = value; mClearColor = Color.White; } }
         #endregion
 
@@ -61,47 +55,25 @@ namespace DragonEngine.SceneManagement
         /// Updatet Funktionen und GameObjects
         /// </summary>
         /// <param name="gameTime"></param>
-        public virtual void Update()
-        {
-            //for (int i = 0; i < mUpdateGameObjects.Count; i++)
-            //    mUpdateGameObjects[i].Update(gameTime);
+        public virtual void Update(GameTime gameTime);
 
-            //for (int i = 0; i < mUpdateAction.Count; i++)
-            //    mUpdateAction[i](gameTime);
+        public virtual void Draw();
+
+        protected void DrawBackground()
+        {
+            EngineSettings.Graphics.GraphicsDevice.SetRenderTarget(mRenderTarget);
+            mSpriteBatch.Begin(SpriteSortMode.Immediate, BlendState.NonPremultiplied, null, null, null, null, mCamera.ViewportTransform);
+            mSpriteBatch.Draw(TextureManager.Instance.GetElementByString<Texture2D>(mBackgroundName), new Rectangle(0, 0, EngineSettings.VirtualResX, EngineSettings.VirtualResY), mClearColor);
+            mSpriteBatch.End();
         }
 
-        public virtual void Draw()
+        protected void DrawToScreen()
         {
-            //EngineSettings.Graphics.GraphicsDevice.SetRenderTarget(mRenderTarget);
-
-            //mSpriteBatch.Begin(SpriteSortMode.Immediate, BlendState.NonPremultiplied, null, null, null, null, mCamera.ViewportTransform);
-            //mSpriteBatch.Draw(TextureManager.Instance.GetElementByString<Texture2D>(mBackgroundName), new Rectangle(0, 0, EngineSettings.VirtualResX, EngineSettings.VirtualResY), mClearColor);
-            //mSpriteBatch.End();
-
-            //for(int i = 0; i < mDrawAction.Count; i++)
-            //    mDrawAction[i]();
-
-            //mSpriteBatch.Begin(SpriteSortMode.Immediate, BlendState.NonPremultiplied, null, null, null, null, mCamera.ViewportTransform);
-            //foreach (GameObject go in mUpdateGameObjects)
-            //    go.Draw(mSpriteBatch);
-            //mSpriteBatch.End();
-
-            //EngineSettings.Graphics.GraphicsDevice.SetRenderTarget(null);
-
-            //mSpriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, null, null, null, null, mCamera.ScreenTransform);
-            //mSpriteBatch.Draw(mRenderTarget, new Rectangle(0, 0, EngineSettings.VirtualResX, EngineSettings.VirtualResY), Color.White);
-            //mSpriteBatch.End();
-
+            EngineSettings.Graphics.GraphicsDevice.SetRenderTarget(null);
+            mSpriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, null, null, null, null, mCamera.ScreenTransform);
+            mSpriteBatch.Draw(mRenderTarget, new Rectangle(0, 0, EngineSettings.VirtualResX, EngineSettings.VirtualResY), Color.White);
+            mSpriteBatch.End();
         }
-
-        ///// <summary>
-        ///// Fügt ein zusätzliches GameObject hinzu, das geupdatet wird.
-        ///// </summary>
-        ///// <param name="go"></param>
-        //protected void AddGameObjectToScene(GameObject go)
-        //{
-        //    mUpdateGameObjects.Add(go);
-        //}
         #endregion
 
     }
