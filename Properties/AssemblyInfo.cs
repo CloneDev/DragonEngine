8befa0fb03076454ae9d845af7f741dffc8b382f
sensorreinigung <jens@richter-fam.de>
2014-04-04 23:20:32 +0200

    [DLL] Bsp deleted. Nur noch dll!
    Da jetzt die Tage jeder am eigenen Klon arbeitet hier als dll, dann kann
    man an der Engine hier weiter basteln, die dll neu bilden und diese
    immer einfach im Klon eingebunden haben, dass sie schön mit aktualisiert
    wird...

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,9 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SnakeMobile")]
-[assembly: AssemblyProduct("SnakeMobile")]
-[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTitle("DragonEngine")]
+[assembly: AssemblyProduct("DragonEngine")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
@@ -16,11 +15,14 @@ using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+// COM, set the ComVisible attribute to true on that type. Only Windows
+// assemblies support COM.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("60180f9a-a437-4d3c-9955-93f093869c9a")]
+// On Windows, the following GUID is for the ID of the typelib if this
+// project is exposed to COM. On other platforms, it unique identifies the
+// title storage container when deploying this assembly to the device.
+[assembly: Guid("99172b2e-f7f8-4288-a44d-9540e9ef394b")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +31,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
