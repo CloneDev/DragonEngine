1cb87dc44ae4cf9a7ee09d5cdf9b07d13884653d
sensorreinigung <jens@richter-fam.de>
2014-04-13 11:51:20 +0200

    GenerateParallaxPlane angelegt

--- DragonEngine/Entities/Parallax/ParallaxSpinePlane.cs
+++ DragonEngine/Entities/Parallax/ParallaxSpinePlane.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using DragonEngine.Pools;
 
 namespace DragonEngine.Entities
 {
-    public class ParallaxSpinePlane
+    public class ParallaxSpinePlane : ParallaxPlane<SpineObject>
     {
+
+        #region Methods
+
+        public void Generate(Dictionary<String, SpinePool> pInterpreter)
+        {
+
+        }
+
+        #endregion
     }
 }
--- DragonEngine/Entities/Parallax/ParallaxSpritePlane.cs
+++ DragonEngine/Entities/Parallax/ParallaxSpritePlane.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using DragonEngine.Pools;
 
 namespace DragonEngine.Entities
 {
     public class ParallaxSpritePlane : ParallaxPlane<Sprite>
     {
+
+        #region Methods
+
+        public void Generate(Dictionary<String, SpritePool> pInterpreter)
+        {
+
+        }
+
+        #endregion
+
     }
 }
