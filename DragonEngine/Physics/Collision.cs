942e4f46b04c6c661e80448ca2f3cded6867ac84
sensorreinigung <jens@richter-fam.de>
2014-04-13 20:55:27 +0200

    Precise Collision w Sliding

--- DragonEngine/Physics/Collision.cs
+++ DragonEngine/Physics/Collision.cs
@@ -17,47 +17,74 @@ namespace DragonEngine.Physics
         
         public static Vector2 CollisionCheckedVector(Rectangle pBody ,int pDeltaX, int pDeltaY, List<Rectangle> pBodiesToCheck)
         {
-            Rectangle TmpCollisionBody = pBody;
-            Vector2 TmpMove = Vector2.Zero;
+            Vector2 TmpTotalMove = new Vector2(pDeltaX, pDeltaY);
+            Rectangle TmpCollisionBody;
+            Vector2 TmpMove;
             int TmpStep;
-            bool TmpCollided;
-            //Größere Koordinate als Iteration nehmen
-            if (Math.Abs(pDeltaX) > Math.Abs(pDeltaY))
-            {
-                TmpStep = Math.Abs(pDeltaX);
-            }
-            else
-            {
-                TmpStep = Math.Abs(pDeltaY);
-            }
-            //Iteration
-            for (int i = 1; i <= TmpStep; i++)
-            {
-                TmpCollided = false;
-                //Box für nächsten Iterationsschritt berechnen
-                TmpCollisionBody.X = pBody.X + ((pDeltaX / TmpStep) * i);
-                TmpCollisionBody.Y = pBody.Y + ((pDeltaY / TmpStep) * i);
-                //Gehe die Blöcke der Liste durch
-                foreach (Rectangle pBodyToCheck in pBodiesToCheck)
+            bool TmpSlide;
+            do
+            { //An Bodies entlang sliden möglich machen.
+                TmpSlide = false;
+                TmpCollisionBody = pBody;
+                TmpMove = Vector2.Zero;
+                //Größere Koordinate als Iteration nehmen
+                if (Math.Abs(TmpTotalMove.X) > Math.Abs(TmpTotalMove.Y))
                 {
-                    //Wenn Kollision vorliegt: Keinen weiteren Block abfragen
-                    if (TmpCollisionBody.Intersects(pBodyToCheck))
+                    TmpStep = (int)Math.Abs(TmpTotalMove.X);
+                }
+                else
+                {
+                    TmpStep = (int)Math.Abs(TmpTotalMove.Y);
+                }
+                //Iteration
+                for (int i = 1; i <= TmpStep; i++)
+                {
+                    //Box für nächsten Iterationsschritt berechnen
+                    TmpCollisionBody.X = (int)(pBody.X + ((TmpTotalMove.X / TmpStep) * i));
+                    TmpCollisionBody.Y = (int)(pBody.Y + ((TmpTotalMove.Y / TmpStep) * i));
+                    if (CollisionCheck(TmpCollisionBody, pBodiesToCheck)) //Bei Kollision: Kollisionsabfrage mit letztem kollisionsfreien Zustand beenden
                     {
-                        TmpCollided = true;
+                        if (i == 1) //Testen ob Sliden möglich ist
+                        {
+                            if (!CollisionCheck(new Rectangle(pBody.X, TmpCollisionBody.Y, TmpCollisionBody.Width, TmpCollisionBody.Height), pBodiesToCheck))
+                            { //Vertikales Sliden
+                                TmpTotalMove.X = 0;
+                                TmpSlide = true;
+                            }
+                            else if (!CollisionCheck(new Rectangle(TmpCollisionBody.X, pBody.Y, TmpCollisionBody.Width, TmpCollisionBody.Height), pBodiesToCheck))
+                            { //Horizontales Sliden
+                                TmpTotalMove.Y = 0;
+                                TmpSlide = true;
+                            }
+                            if (TmpTotalMove.X == 0 && TmpTotalMove.Y == 0)
+                                TmpSlide = false; //Endlosschleife verhindern
+                        }
                         break;
                     }
+                    else //Kollisionsfreien Fortschritt speichern
+                    {
+                        TmpMove.X = TmpCollisionBody.X - pBody.X;
+                        TmpMove.Y = TmpCollisionBody.Y - pBody.Y;
+                    }
                 }
-                if (TmpCollided == true) //Bei Kollision: Kollisionsabfrage mit letztem kollisionsfreien Zustand beenden
+            } while (TmpSlide);
+            return TmpMove;
+        }
+
+        protected static bool CollisionCheck(Rectangle pBodyToCheck, List<Rectangle> pBodiesToCheck)
+        {
+            bool TmpCollision = false;
+            //Gehe die Blöcke der Liste durch
+            foreach (Rectangle TmpBodyToCheck in pBodiesToCheck)
+            {
+                //Wenn Kollision vorliegt: Keinen weiteren Block abfragen
+                if (pBodyToCheck.Intersects(TmpBodyToCheck))
                 {
+                    TmpCollision = true;
                     break;
                 }
-                else //Kollisionsfreien Fortschritt speichern
-                {
-                    TmpMove.X = TmpCollisionBody.X - pBody.X;
-                    TmpMove.Y = TmpCollisionBody.Y - pBody.Y;
-                }
             }
-            return TmpMove;
+            return TmpCollision;
         }
 
         #endregion
