76776f092730bce8486a889383fa9bedfffba604
sensorreinigung <jens@richter-fam.de>
2014-04-14 09:11:05 +0200

    Rest von Buttons merge

--- DragonEngine/Entities/Sprite.cs
+++ DragonEngine/Entities/Sprite.cs
@@ -76,11 +76,17 @@ namespace DragonEngine.Entities
 
         #endregion
 
-        public void UpdateTextureDimensions()
+        protected void UpdateTextureDimensions()
         {
             mWidth = TextureManager.Instance.GetElementByString<Texture2D>(mTextureName).Width;
             mHeight = TextureManager.Instance.GetElementByString<Texture2D>(mTextureName).Height;
             mOrigin = new Vector2(Width / 2, Height / 2);
+            mCollisionBox = new Rectangle(PositionX, PositionY, Width, Height);
+        }
+
+        public void SetPosition(Vector2 pPosition)
+        {
+            this.Position = pPosition;
             mCollisionBox = new Rectangle(PositionX, PositionY, Width, Height);
         }
 
