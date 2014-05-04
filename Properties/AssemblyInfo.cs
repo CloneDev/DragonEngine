1e4fd1da2b23831ec8795f0d1d2ca09351a9faea
sensorreinigung <jens@richter-fam.de>
2014-05-04 11:21:12 +0200

    MonoGame -> XNA
    Es gibt nen funktionierenden WorkAround für Fullscreen.
    Kann man ja dann machen falls der Shaderbug Probleme macht.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 // On Windows, the following GUID is for the ID of the typelib if this
 // project is exposed to COM. On other platforms, it unique identifies the
 // title storage container when deploying this assembly to the device.
-[assembly: Guid("99172b2e-f7f8-4288-a44d-9540e9ef394b")]
+[assembly: Guid("f3518438-c5bc-4fdd-be62-148c941fac02")]
 
 // Version information for an assembly consists of the following four values:
 //
